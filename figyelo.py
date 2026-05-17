fd7ad114f97522c4e6f4c0ff6c33252880df1aca
Mihály Orsós <ormraat.pte@gmail.com>
2026-05-17 20:47:42 +0200

    Refactor message sending to Telegram API

--- figyelo.py
+++ figyelo.py
@@ -27,9 +27,10 @@ print(str(response.content))
 if "jelenleg nem" in str(response.content):
     print("küldés")
     telegram_url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
-    requests.post(telegram_url, json={
+    kuldes = requests.post(telegram_url, json={
         "chat_id": CHAT_ID,
-        "text": message
+        "text": "mehet"
     })
+    print(kuldes)
 
     print("Message sent!")
